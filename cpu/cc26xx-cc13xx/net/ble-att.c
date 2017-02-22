0079218e7c9133c7e819e2705b45672fd550b960
Arthur Courtel <arthur.courtel@openwide.fr>
2017-02-22 18:16:52 +0100

    add all ATT define opcodes

--- cpu/cc26xx-cc13xx/net/ble-att.c
+++ cpu/cc26xx-cc13xx/net/ble-att.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, Arthur Courtel
+ * Copyright (c) 2017, Arthur Courtel
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -37,48 +37,122 @@
 #include "net/netstack.h"
 #include "net/packetbuf.h"
 
-#define L2CAP_ATT_CHANNEL           0x04
+#define L2CAP_ATT_CHANNEL                 0x04
 
-#define ATT_MTU_REQUEST             0x02
-//#define ATT_MTU_RESPONSE            0x03
-#define ATT_MTU_RESPONSE_LEN        0x03
+/*---------------------------------------------------------------------------*/
+/* opcode from spec v4.2 p 2201 and v5 p2211 */
+#define ATT_ERROR_RESPONSE                0x01 /* IMPLEMENTED*/
 
-void send_mtu_resp(){
+#define ATT_MTU_REQUEST                   0x02 /* IMPLEMENTED*/
+#define ATT_MTU_RESPONSE                  0x03 /* IMPLEMENTED*/
+
+#define ATT_INFORMATION_REQUEST           0x04 /* NOT IMPLEMENTED*/
+#define ATT_INFORMATION_RESPONSE          0x05 /* NOT IMPLEMENTED*/
+
+#define ATT_FIND_INFO_BY_TYPE_REQUEST     0x06 /* NOT IMPLEMENTED*/
+#define ATT_FIND_INFO_BY_TYPE_RESPONSE    0x07 /* NOT IMPLEMENTED*/
+
+#define ATT_READ_BY_TYPE_REQUEST          0x08 /* NOT IMPLEMENTED*/
+#define ATT_READ_BY_TYPE_RESPONSE         0x09 /* NOT IMPLEMENTED*/
+
+#define ATT_READ_REQUEST                  0x0A /* NOT IMPLEMENTED*/
+#define ATT_READ_RESPONSE                 0x0B /* NOT IMPLEMENTED*/
+
+#define ATT_READ_BLOB_REQUEST             0x0C /* NOT IMPLEMENTED*/
+#define ATT_READ_BLOB_RESPONSE            0x0D /* NOT IMPLEMENTED*/
+
+#define ATT_READ_MULTIPLE_REQUEST         0x0E /* NOT IMPLEMENTED*/
+#define ATT_READ_MULTIPLE_RESPONSE        0x0F /* NOT IMPLEMENTED*/
+
+#define ATT_READ_BY_GROUP_TYPE_REQUEST    0x10 /* NOT IMPLEMENTED*/
+#define ATT_READ_BY_GROUP_TYPE_RESPONSE   0x11 /* NOT IMPLEMENTED*/
+
+#define ATT_WRITE_REQUEST                 0x12 /* NOT IMPLEMENTED*/
+#define ATT_WRITE_RESPONSE                0x13 /* NOT IMPLEMENTED*/
+
+#define ATT_WRITE_COMMAND_REQUEST         0x52 /* NOT IMPLEMENTED*/
+
+#define ATT_PREPARE_WRITE_REQUEST         0x16 /* NOT IMPLEMENTED*/
+#define ATT_PREPARE_WRITE_RESPONSE        0x17 /* NOT IMPLEMENTED*/
+
+#define ATT_EXECUTE_WRITE_REQUEST         0x18 /* NOT IMPLEMENTED*/
+#define ATT_EXECUTE_WRITE_RESPONSE        0x19 /* NOT IMPLEMENTED*/
+
+#define ATT_HANDLE_VALUE_NOTIFICATION     0x1B /* NOT IMPLEMENTED*/
+#define ATT_HANDLE_VALUE_INDICATION       0x1D /* NOT IMPLEMENTED*/
+#define ATT_HANDLE_VALUE_CONFIRMATION     0x1E /* NOT IMPLEMENTED*/
+
+#define ATT_SIGNED_WRITE_COMMAND          0xD2 /* NOT IMPLEMENTED*/
+/*---------------------------------------------------------------------------*/
+
+#define ATT_MTU_RESPONSE_LEN              0x03
+#define ATT_ERROR_RESPONSE_LEN            0x05
+
+
+/*---------------------------------------------------------------------------*/
+enum error_code {
+  INVALID_HANDLE,
+  READ_NOT_PERMITTED,
+  WRITE_NOT_PERMITTED,
+  INVALID_PDU,
+  INSUFFICIENT AUTHENTICATION,
+  REQUEST_NOT_SUPPORTED
+}
+
+/*---------------------------------------------------------------------------*/
+static void send_mtu_resp(){
 
   uint8_t data[ATT_MTU_RESPONSE_LEN];
 
-  data[0]= ATT_MTU_RESPONSE_LEN;
+  data[0]= ATT_MTU_RESPONSE;
 
   /* Server Rx MTU */
   data[1]=0x17;
   data[2]=0x00;
+
   memcpy(packetbuf_dataptr(), data,ATT_MTU_RESPONSE_LEN);
   packetbuf_set_datalen(ATT_MTU_RESPONSE_LEN);
-  //packetbuf_copyfrom((void *)data, 3);
+
+  NETSTACK_MAC.send(NULL, NULL);
+}
+/*---------------------------------------------------------------------------*/
+/* NOT TESTED */
+static void send_error_resp(uint8_t* opcode, uint8_t error)
+{
+  uint8_t data[ATT_ERROR_RESPONSE_LEN];
+
+  data[0] = ATT_ERROR_RESPONSE;
+
+  /* Server Rx MTU */
+  data[1] = opcode;
+  data[2] = 0x00;
+  data[3] = 0x00;
+  data[4] = error;
+
+  memcpy(packetbuf_dataptr(), data,ATT_ERROR_RESPONSE_LEN);
+  packetbuf_set_datalen(ATT_ERROR_RESPONSE_LEN);
+
   NETSTACK_MAC.send(NULL, NULL);
 }
 
-
-void input_att(void){
+/*---------------------------------------------------------------------------*/
+static void input_att(void){
 
   uint8_t *data = (uint8_t *)packetbuf_dataptr();
   //uint8_t len = packetbuf_datalen();
-  // while (--len>0){
-  //   printf("Data : 0x%x", *data++);
-  //
-  // }/*
   switch (data[0]){
     case ATT_MTU_REQUEST:
       send_mtu_resp();
       break;
     default :
-      printf("Opcode number 0x%x not available", data[1]);
+
+      send_error_resp(data[0], REQUEST_NOT_SUPPORTED);
       break;
   }
 }
 
 static void init(void){
-
+  /* Nothing to do for now */
 }
 
 const struct network_driver gatt_driver =
