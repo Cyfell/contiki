010cec247299831ea94ddccb78bf94ba2393a9cc
Arthur Courtel <arthur.courtel@openwide.fr>
2017-04-14 15:16:52 +0200

    Add Battery notifications

--- cpu/cc26xx-cc13xx/net/profiles/battery.c
+++ cpu/cc26xx-cc13xx/net/profiles/battery.c
@@ -44,9 +44,19 @@
 #include "battery.h"
 #include "board-peripherals.h"
 #include "batmon-sensor.h"
+#include "notify.h"
+#include "ble-hal-cc26xx.h"
+/* process for temp notification */
+PROCESS(battery_notify_process, "battery_notify_process");
+/* process callback on disconnect event */
+PROCESS(battery_disconnect_process, "Disconnect battery notify");
+
+static uint16_t handle_to_notify;
+static bt_size_t previous_value;
+static uint32_t period_notify;
 
 /*---------------------------------------------------------------------------*/
-uint8_t get_battery_info(bt_size_t *database){
+uint8_t get_value_battery(bt_size_t *database){
   int value;
   uint16_t tmp;
 
@@ -74,7 +84,7 @@ uint8_t get_battery_info(bt_size_t *database){
   return SUCCESS;
 }
 /*---------------------------------------------------------------------------*/
-uint8_t enable_disable_battery(const bt_size_t * new_value){
+uint8_t set_status_battery_sensor(const bt_size_t * new_value){
   switch(new_value->value.u8){
     case 1:
       PRINTF("ACTIVATION CAPTEUR\n");
@@ -90,9 +100,116 @@ uint8_t enable_disable_battery(const bt_size_t * new_value){
   return SUCCESS;
 }
 /*---------------------------------------------------------------------------*/
-uint8_t get_status_battery(bt_size_t *database){
+uint8_t get_status_battery_sensor(bt_size_t *database){
   database->type = BT_SIZE8;
   database->value.u8 = (uint8_t) batmon_sensor.status(SENSORS_ACTIVE);
   PRINTF("status temp sensor : 0x%X\n", batmon_sensor.status(SENSORS_ACTIVE));
   return SUCCESS;
+}
+/*---------------------------------------------------------------------------*/
+uint8_t get_status_battery_notify(bt_size_t *status_value){
+  status_value->type = BT_SIZE8;
+  if (process_is_running(&battery_notify_process) == 0){
+      status_value->value.u8 = 0;
+  }else{
+    status_value->value.u8 = 1;
+  }
+
+  return SUCCESS;
+}
+/*---------------------------------------------------------------------------*/
+static inline void enable_notification(){
+  PRINTF("ACTIVATION battery NOTIFICATIONS\n");
+  handle_to_notify = g_current_att->att_value.value.u16;
+  process_start(&battery_notify_process, NULL);
+  process_start(&battery_disconnect_process, NULL);
+}
+/*---------------------------------------------------------------------------*/
+static inline void disable_notification(){
+  PRINTF("DESACTIVATION battery NOTIFICATIONS\n");
+  process_exit(&battery_notify_process);
+  process_exit(&battery_disconnect_process);
+}
+/*---------------------------------------------------------------------------*/
+uint8_t set_status_battery_notify(const bt_size_t *new_value){
+uint8_t error;
+error = SUCCESS;
+  switch(new_value->value.u8){
+    case 1:
+    enable_notification();
+      break;
+    case 0:
+    disable_notification();
+      break;
+    default:
+      return ATT_ECODE_BAD_NUMBER; //ERROR
+  }
+
+  if (error != SUCCESS)
+    return error;
+
+  return SUCCESS;
+}
+/*---------------------------------------------------------------------------*/
+uint8_t set_period_battery(const bt_size_t *new_period){
+  /* convert period received in system seconds */
+  period_notify = (swap32(new_period->value.u32)) * CLOCK_SECOND;
+  /* period mini = CLOCK_SECOND */
+  if (period_notify < (uint32_t) CLOCK_SECOND)
+    period_notify = (uint32_t) CLOCK_SECOND;
+
+  PRINTF("new period : %lX\n", period_notify);
+  return SUCCESS;
+}
+/*---------------------------------------------------------------------------*/
+uint8_t get_period_battery(bt_size_t *period_to_send){
+  /* convert period in seconds */
+  period_to_send->value.u32 = (period_notify)/CLOCK_SECOND;
+  period_to_send->type = BT_SIZE32;
+  return SUCCESS;
+}
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(battery_notify_process, ev, data)
+{
+  static struct etimer notify_timer;
+  bt_size_t sensor_value;
+  uint8_t error;
+
+  PROCESS_BEGIN();
+  /* initiate notify period to CLOCK_SECOND */
+  period_notify = CLOCK_SECOND;
+  etimer_set(&notify_timer, (clock_time_t) period_notify);
+
+  while(1){
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&notify_timer));
+    /* update notification period with possible new period */
+    etimer_reset_with_new_interval(&notify_timer, (clock_time_t) period_notify);
+
+        error = get_value_battery(&sensor_value);
+        if (is_values_equals(&sensor_value, &previous_value) != 0){
+          if (error != SUCCESS){
+            prepare_error_resp_notif(handle_to_notify, error);
+            /* If error, disable notifications */
+            disable_notification();
+          }else {
+            prepare_notification(handle_to_notify, &sensor_value);
+            previous_value = sensor_value;
+          }
+
+          send_notify();
+        }
+  }
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
+// Disable notifications when disconnection event show up
+PROCESS_THREAD(battery_disconnect_process, ev, data){
+
+    PROCESS_BEGIN();
+
+      while(1){
+        PROCESS_WAIT_EVENT_UNTIL(ev == ll_disconnect_event);
+        disable_notification();
+      }
+  PROCESS_END();
 }
--- cpu/cc26xx-cc13xx/net/profiles/battery.h
+++ cpu/cc26xx-cc13xx/net/profiles/battery.h
@@ -35,9 +35,18 @@
 #define BATTERY_H_
 
 #include "net/att-database.h"
+#include "process.h"
 /*---------------------------------------------------------------------------*/
-uint8_t get_battery_info(bt_size_t *database);
-uint8_t enable_disable_battery(const bt_size_t *new_value);
-uint8_t get_status_battery(bt_size_t *database);
+uint8_t get_value_battery(bt_size_t *value);
+uint8_t set_status_battery_sensor(const bt_size_t *new_value);
+uint8_t get_status_battery_sensor(bt_size_t *database);
+/* notification functions */
+uint8_t set_period_battery(const bt_size_t *new_period);
+uint8_t get_period_battery(bt_size_t *period_to_send);
+uint8_t set_status_battery_notify(const bt_size_t *new_value);
+uint8_t get_status_battery_notify(bt_size_t *database);
+/* notify process */
+PROCESS_NAME(battery_notify_process);
+PROCESS_NAME(battery_disconnect_process);
 /*---------------------------------------------------------------------------*/
 #endif  // BATTERY_H_
